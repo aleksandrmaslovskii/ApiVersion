aeb069ee7abe7a3c159cd0bf59fc32c5d557543c
aleksandrmaslovskii <aleksandr.maslovskii.1990@gmail.com>
2022-03-13 21:30:28 +0300

    Update versioning

--- ApiVersion/Controllers/V2/WeatherForecastController.cs
+++ ApiVersion/Controllers/V2/WeatherForecastController.cs
@@ -7,27 +7,14 @@ namespace ApiVersion.Controllers.V2;
 [ApiVersion("2.0")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    [HttpGet]
+    public WeatherForecast Get()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return new WeatherForecast
+        {
+            Date = DateTime.Now,
+            TemperatureC = 10,
+            Summary = "Bracing"
+        };
     }
 }
--- ApiVersion/Controllers/WeatherForecastController.cs
+++ ApiVersion/Controllers/WeatherForecastController.cs
@@ -3,31 +3,18 @@ using Microsoft.AspNetCore.Mvc;
 namespace ApiVersion.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 [ApiVersion("1.0", Deprecated = true)]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    [HttpGet]
+    public WeatherForecast Get()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        return new WeatherForecast
+        {
+            Date = DateTime.Now,
+            TemperatureC = 0,
+            Summary = "Freezing"
+        };
     }
 }
